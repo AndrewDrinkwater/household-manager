37eebfd28bd2b7b49aa67acb410a23f215ac0c5c
AndrewDrinkwater <38835963+AndrewDrinkwater@users.noreply.github.com>
2025-06-14 13:29:53 +0100

    Fix password comment formatting

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -5,7 +5,7 @@ const sequelize = require('../db');
 const User = sequelize.define('User', {
   username: { type: DataTypes.STRING, allowNull: false, unique: true },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },  // Added password field
+  password: { type: DataTypes.STRING, allowNull: false }, // Added password field
   role: { type: DataTypes.ENUM('user', 'admin'), allowNull: false, defaultValue: 'user' }
 });
 
